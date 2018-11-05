eaf9462401616505db4ccecf8448784ab1fac630
Milan Masnikosa <milan.masnikosa@gmail.com>
2018-11-05 17:30:39 +0100

    initial project

---
+++ AccountManagement/iAccountManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountManagement
+{
+    public interface iAccountManager
+    {
+        void AddAccount();
+        void Delete();
+    }
+}
---
+++ AccountManagement/iPayment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountManagement
+{
+    public interface iPayment
+    {
+        void Pay(double accountNumber,double sum);
+        void PayOff(double accountNumber, double sum);
+    }
+}
