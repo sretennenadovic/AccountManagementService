07379266a49d674e79486345316be4f3680e6908
asd <asd@gmail.com>
2018-11-06 16:50:01 +0100

    Client first commit

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,27 @@ namespace Client
     {
         static void Main(string[] args)
         {
+            string option;
+            string srvCertCN = String.Empty;
+            Console.WriteLine("Choose one option:");
+            option = Console.ReadLine();
+            switch(option)
+            {
+                case "1":
+                    srvCertCN = "AMSWEL";
+                    break;
+
+                case "2":
+                    srvCertCN = "AMSXML";
+                    break;
+            }
+
+            NetTcpBinding binding = new NetTcpBinding();
+            binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Certificate;
+
+            /// Use CertManager class to obtain the certificate based on the "srvCertCN" representing the expected service identity.
+            X509Certificate2 srvCert = CertManager.GetCertificateFromStorage(StoreName.My, StoreLocation.LocalMachine, srvCertCN);
+            EndpointAddress address = new EndpointAddress(new Uri("net.tcp://localhost:9999/Receiver"), new X509CertificateEndpointIdentity(srvCert));
         }
     }
 }
